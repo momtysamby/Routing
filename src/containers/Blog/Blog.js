2151e4d1788957e0119d0433262fb05827459417
momtysamby <momtysamby@gmail.com>
2020-12-27 19:59:00 +0300

    Bug with rendering unnecessary elements resolved

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 import NewPost from './NewPost/NewPost'
 import FullPost from './FullPost/FullPost'
 
@@ -12,7 +12,11 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to='/' exact>Home</NavLink></li>
+                            <li><NavLink 
+                            to='/' 
+                            exact
+                            activeStyle={{textDecoration : 'underline'}}
+                            >Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post'
                             }}>New Post</NavLink></li>
@@ -20,9 +24,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path='/' exact render={() => <h1>Home</h1>}/> */}
-                <Route path='/' exact component={Posts} />
-                <Route path='/new-post' exact component={NewPost} />
-                <Route path='/:id' exact component={FullPost} />
+                <Switch>
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' exact component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
+                
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -30,18 +30,20 @@ class Posts extends Component {
 
     postSelectedHandler = (id) => {
         this.setState({selectedPostId: id});
+        //this.props.history.push({pathname: '/' + id})
     }
 
     render() {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Link to={'/' + post.id} key={post.id}>
-                        <Post  
+                return (
+                <Link to={'/' + post.id} key={post.id}>
+                    <Post  
                         title={post.title} 
                         author={post.author}
                         clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>;
+                </Link>)
             });
         }
         return (
